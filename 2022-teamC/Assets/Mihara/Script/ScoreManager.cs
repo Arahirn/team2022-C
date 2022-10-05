105bd0c9a1bedb216218ee3ef91d96fb5ec428b2
Morita-340 <109466695+Morita-340@users.noreply.github.com>
2022-10-05 23:54:07 +0900

    か

---
+++ 2022-teamC/Assets/Mihara/Script/ScoreManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    
+
+    public GameObject targetObject;
+
+    float HighScore = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Transform a = targetObject.transform;
+
+        Vector3 Camerapos = a.position;
+        var camy = Camerapos.y;
+        if (camy > HighScore)
+        {
+            HighScore = camy;
+        }
+        this.GetComponent<Text>().text = HighScore.ToString("F2") + "m";
+
+    }
+   
+}
---
+++ 2022-teamC/Assets/Morita/MaxHeightScoreUI.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MaxHeightScoreUI : MonoBehaviour
+{
+    public RayCastScriptA rcsa;
+    public RayCastScriptB rcsb;
+    public RayCastScriptC rcsc;
+    public RayCastScriptD rcsd;
+    public RayCastScriptE rcse;
+    public RayCastScriptF rcsf;
+    public RayCastScriptG rcsg;
+    public float max;
+    string MAX;
+    Text text;
+    float HighScore;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        HighScore = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject ra = GameObject.Find("Ray A"); //Ray Aっていうオブジェクトを探す
+        rcsa = ra.GetComponent<RayCastScriptA>();　//付いているスクリプトを取得
+        GameObject rb = GameObject.Find("Ray B");
+        rcsb = rb.GetComponent<RayCastScriptB>();
+        GameObject rc = GameObject.Find("Ray C");
+        rcsc = rc.GetComponent<RayCastScriptC>();
+        GameObject rd = GameObject.Find("Ray D");
+        rcsd = rd.GetComponent<RayCastScriptD>();
+        GameObject re = GameObject.Find("Ray E");
+        rcse = re.GetComponent<RayCastScriptE>();
+        GameObject rf = GameObject.Find("Ray F");
+        rcsf = rf.GetComponent<RayCastScriptF>();
+        GameObject rg = GameObject.Find("Ray G");
+        rcsg = rg.GetComponent<RayCastScriptG>();
+        text = GameObject.Find("Text (Legacy)").GetComponent <Text>();
+        max = Mathf.Max(rcsa.YofA, rcsb.YofB, rcsc.YofC, rcsd.YofD, rcse.YofE, rcsf.YofF, rcsg.YofG);
+        if (max > HighScore)
+        {
+            HighScore = max;
+        }
+        MAX = HighScore.ToString();
+        this.text.text = MAX;
+    }
+}
