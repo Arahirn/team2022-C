93bc1a589025a429d37335b66a9b7dae78bd6874
Morita-340 <109466695+Morita-340@users.noreply.github.com>
2022-10-11 00:26:22 +0900

    諸修正＋起動時の音処理実装

--- 2022-teamC/Assets/Mihara/Script/StartController.cs
+++ 2022-teamC/Assets/Mihara/Script/StartController.cs
@@ -1,23 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class Startcontroller : MonoBehaviour
+public class StartController : MonoBehaviour
 {
+    public AudioClip BubbleSound;
+    private AudioSource audioSource;
+    public GameObject Panel;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        audioSource = gameObject.GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     public void PushButton()
     {
+        StartCoroutine("SceneChangeManager1");
+        
+    }
+    private IEnumerator SceneChangeManager1()
+    {
+        audioSource.PlayOneShot(BubbleSound);
+        //泡の音を鳴らす
+        Panel.SetActive(true);
+        //フェードアウトする
+        yield return new WaitForSeconds(2.0f);
+        // 2秒待つ
         SceneManager.LoadScene("Game");
+
     }
 }
