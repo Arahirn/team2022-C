bf3c72b157a09c0f94eb829f9866cdca818a6382
mihara111 <109466642+mihara111@users.noreply.github.com>
2022-10-13 02:25:30 +0900

    202210130225

---
+++ 2022-teamC/Assets/Mihara/Script/Gameendmanager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Gameendmanager : MonoBehaviour
+{
+   
+    public void PushButton()
+    {
+        SceneManager.LoadScene("Title");
+
+    }
+
+}
